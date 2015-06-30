4a1f63a1f0a59de934c3e1e89d1887d4179af191
Ferdinand Malcher (pi35) <mail@ferdinand-malcher.de>
2015-06-30 11:43:05 +0000

    initial store.json and existence check

--- modules/store.js
+++ modules/store.js
@@ -1,18 +1,34 @@
 var fs = require('fs');
-var touch = require('touch');
 
-touch('store.json');
+var storefile = 'store.json';
+var initialfile = 'store.json.initial';
+
+
+//initialize store file
+fs.exists(storefile, function(exists){
+	if(!exists){
+		//read initial file
+		fs.readFile(initialfile, 'utf8', function(err, data) {
+			//write to store.json
+			fs.writeFile(storefile, data, function(err) {
+				
+			});
+		});
+	}
+})
+
+
 
 
 exports.set = function(data, callback) {
-	fs.writeFile('store.json', JSON.stringify(data), function(err) {
+	fs.writeFile(storefile, JSON.stringify(data), function(err) {
 		callback(err);
 	});
 }
 
 
 exports.get = function(callback){
-	fs.readFile('store.json', 'utf8', function(err, data) {
+	fs.readFile(storefile, 'utf8', function(err, data) {
 		return callback(err, JSON.parse(data));
 	});
 }
